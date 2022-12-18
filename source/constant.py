e78c3c23be12a70caf276547c41ea610d1aacfe5
eroltskn <eroltaskinnn@gmail.com>
2022-12-18 23:24:40 +0300

    Add logout button

--- source/constant.py
+++ source/constant.py
@@ -1,6 +1,6 @@
 import os
 
 class Constant(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:0EROLtaskin@localhost/myth_database'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'any secret string'
